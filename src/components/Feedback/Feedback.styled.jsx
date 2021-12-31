94796ce03bb1574171e8fdd8d053b11679bd67bf
Tsimura <chappa2828@gmail.com>
2021-12-31 23:05:39 +0200

    Update Feedback.styled.jsx

--- src/components/Feedback/Feedback.styled.jsx
+++ src/components/Feedback/Feedback.styled.jsx
@@ -5,17 +5,18 @@ export const FeedbackWrapper = styled.section`
   display: flex;
   flex-direction: column;
   align-items: center;
-  margin-top: 10px;
+  margin-top: 40px;
   margin-left: auto;
   margin-right: auto;
   padding: 10px;
-  width: 740px;
-  height: 400px;
+  width: 780px;
+  height: 500px;
   border-radius: 5px;
   box-shadow: 0px 1px 3px rgba(0, 0, 0, 0.12), 0px 1px 1px rgba(0, 0, 0, 0.14),
     0px 2px 1px rgba(0, 0, 0, 0.2);
   background-image: url('https://cdn.pixabay.com/photo/2017/01/12/14/22/coffee-1974841_960_720.jpg');
   background-repeat: no-repeat;
   background-position: bottom;
+  background-size: cover;
   background-color: rgb(135 132 132);
 `;

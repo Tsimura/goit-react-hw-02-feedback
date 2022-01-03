e5c6e8bb411f3779a17feca610b2d234281107a8
Tsimura <chappa2828@gmail.com>
2022-01-03 12:20:38 +0200

    add button:hover

--- src/components/FeedbackOptions/FeedbackOptions.styled.jsx
+++ src/components/FeedbackOptions/FeedbackOptions.styled.jsx
@@ -13,24 +13,24 @@ export const FeedbackOptionsBtn = styled.div`
     transition: background-color 250ms cubic-bezier(0.4, 0, 0.2, 1),
       color 250ms cubic-bezier(0.4, 0, 0.2, 1),
       box-shadow 250ms cubic-bezier(0.4, 0, 0.2, 1);
+    &:not(:last-child) {
+      margin-right: 34px;
+    }
   }
   button:hover {
     color: #ffffff;
     box-shadow: 0px 3px 1px rgba(0, 0, 0, 0.1), 0px 1px 2px rgba(0, 0, 0, 0.08),
       0px 2px 2px rgba(0, 0, 0, 0.12);
     border-radius: 4px;
-  }
-  button:hover:nth-child(1) {
-    background: green;
-  }
-  button:hover:nth-child(2) {
-    color: #000;
-    background: yellow;
-  }
-  button:hover:nth-child(3) {
-    background: red;
-  }
-  button:not(:last-child) {
-    margin-right: 34px;
+    &:nth-child(1) {
+      background: green;
+    }
+    &:nth-child(2) {
+      color: #000;
+      background: yellow;
+    }
+    &:nth-child(3) {
+      background: red;
+    }
   }
 `;
